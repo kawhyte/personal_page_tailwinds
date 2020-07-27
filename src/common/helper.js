36c0b7b2ecdb4aa96734b6d0ce3abb6f86a5a430
Kenny <kawhyte@gmail.com>
2020-07-27 13:01:31 -0700

    added icons

--- src/common/helper.js
+++ src/common/helper.js
@@ -107,154 +107,18 @@ const iconList = {
         Arduino: {
           name: "Arduino",
           icon:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505251/flags/Flag_of_Massachusetts.svg"
+            "https://res.cloudinary.com/babyhulk/image/upload/c_scale,q_auto,w_64/v1595879735/Tech_icons/arduino.webp"
         },
         RFID: {
           name: "RFID",
           icon:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505251/flags/Flag_of_Michigan.svg"
+            "https://res.cloudinary.com/babyhulk/image/upload/c_scale,q_auto,w_64/v1595879814/Tech_icons/rfid.webp"
         },
         SQL: {
           name: "SQL",
           icon:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505251/flags/Flag_of_Minnesota.svg"
-        },
-        MS: {
-          name: "Mississippi",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505253/flags/Flag_of_Mississippi.svg"
-        },
-        MO: {
-          name: "Missouri",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505253/flags/Flag_of_Missouri.svg"
-        },
-        MT: {
-          name: "Montana",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505253/flags/Flag_of_Montana.svg"
-        },
-        NE: {
-          name: "Nebraska",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505254/flags/Flag_of_Nebraska.svg"
-        },
-        NV: {
-          name: "Nevada",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505254/flags/Flag_of_Nevada.svg"
-        },
-        NH: {
-          name: "New Hampshire",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505255/flags/Flag_of_New Hampshire.svg"
-        },
-        NJ: {
-          name: "New Jersey",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505256/flags/Flag_of_New Jersey.svg"
-        },
-        NM: {
-          name: "New Mexico",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505256/flags/Flag_of_New Mexico.svg"
-        },
-        NY: {
-          name: "New York",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505256/flags/Flag_of_New York.svg"
-        },
-        NC: {
-          name: "North Carolina",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505256/flags/Flag_of_North Carolina.svg"
-        },
-        ND: {
-          name: "North Dakota",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505256/flags/Flag_of_North Dakota.svg"
-        },
-        OH: {
-          name: "Ohio",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505257/flags/Flag_of_Ohio.svg"
-        },
-        OK: {
-          name: "Oklahoma",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505259/flags/Flag_of_Oklahoma.svg"
-        },
-        OR: {
-          name: "Oregon",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505259/flags/Flag_of_Oregon.svg"
-        },
-        PA: {
-          name: "Pennsylvania",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505259/flags/Flag_of_Pennsylvania.svg"
-        },
-        RI: {
-          name: "Rhode Island",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505259/flags/Flag_of_Rhode Island.svg"
-        },
-        SC: {
-          name: "South Carolina",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505259/flags/Flag_of_South Carolina.svg"
-        },
-        SD: {
-          name: "South Dakota",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505259/flags/Flag_of_South Dakota.svg"
-        },
-        TN: {
-          name: "Tennessee",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505261/flags/Flag_of_Tennessee.svg"
-        },
-        TX: {
-          name: "Texas",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505261/flags/Flag_of_Texas.svg"
-        },
-        UT: {
-          name: "Utah",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505261/flags/Flag_of_Utah.svg"
-        },
-        VT: {
-          name: "Vermont",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505261/flags/Flag_of_Vermont.svg"
-        },
-        VA: {
-          name: "Virginia",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505262/flags/Flag_of_Virginia.svg"
-        },
-        WA: {
-          name: "Washington",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505263/flags/Flag_of_Washington.svg"
-        },
-        WV: {
-          name: "West Virginia",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505263/flags/Flag_of_West Virginia.svg"
-        },
-        WI: {
-          name: "Wisconsin",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505263/flags/Flag_of_Wisconsin.svg"
-        },
-        WY: {
-          name: "Wyoming",
-          flag:
-            "https://res.cloudinary.com/babyhulk/image/upload/v1584505263/flags/Flag_of_Wyoming.svg"
-        },
-        ALL: { name: "View All States", flag: "Flag_of_Wyoming.svg" }
+            "https://res.cloudinary.com/babyhulk/image/upload/q_auto/v1595879896/Tech_icons/sql.webp"
+        }
       };
 
       
